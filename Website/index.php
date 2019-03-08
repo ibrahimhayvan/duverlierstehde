ef11cb8a75e424cf59ba84b7d913c5e7a4d761c2
ibrahimhayvan <47698186+ibrahimhayvan@users.noreply.github.com>
2019-03-08 15:43:29 +0100

    Add files via upload

---
+++ Website/index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+
+
+
+<html lang="de">
+<head>
+
+<link rel="shortcut icon" type="image/x-icon" href="/_icons/favicon.ico">
+<link rel="icon" type="image/x-icon" href="/_icons/favicon.ico">
+<link rel="icon" type="image/gif" href="/_icons/favicon.gif">
+<link rel="icon" type="image/png" href="/_icons/favicon.png">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon.png">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-57x57.png" sizes="57x57">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-60x60.png" sizes="60x60">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-72x72.png" sizes="72x72">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-76x76.png" sizes="76x76">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-114x114.png" sizes="114x114">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-120x120.png" sizes="120x120">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-128x128.png" sizes="128x128">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-144x144.png" sizes="144x144">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-152x152.png" sizes="152x152">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-180x180.png" sizes="180x180">
+<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-precomposed.png">
+<link rel="icon" type="image/png" href="/_icons/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/_icons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="/_icons/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/_icons/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="/_icons/favicon-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/_icons/favicon-196x196.png" sizes="196x196">
+<meta name="msapplication-TileImage" content="/_icons/win8-tile-144x144.png"> 
+<meta name="msapplication-TileColor" content="#ffffff"> 
+<meta name="msapplication-navbutton-color" content="#ffffff"> 
+<meta name="application-name" content="Name der Applikation/Webseite"/> 
+<meta name="msapplication-tooltip" content="Name der Applikation/Webseite"/> 
+<meta name="apple-mobile-web-app-title" content="Name der Applikation/Webseite"/> 
+<meta name="msapplication-starturl" content="http://web-stuebchen.de/"/> 
+<meta name="msapplication-square70x70logo" content="/_icons/win8-tile-70x70.png"> 
+<meta name="msapplication-square144x144logo" content="/_icons/win8-tile-144x144.png"> 
+<meta name="msapplication-square150x150logo" content="/_icons/win8-tile-150x150.png"> 
+<meta name="msapplication-wide310x150logo" content="/_icons/win8-tile-310x150.png"> 
+<meta name="msapplication-square310x310logo" content="/_icons/win8-tile-310x310.png"> 
+
+	
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<meta name="google-site-verification" content="7sOZBnLncSRXT8aKD2xHbidmgNhNJOBUFEnYfBeUXsA"/>
+	<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+	<title>Versuch dein Glück</title>
+    <meta charset="utf-8">
+	<link rel="stylesheet" href="index.css">
+
+</head>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+ 									  						  
+<body>
+
+	<header id="headerr"> 
+			<h1> Bist du dabei?</h1>
+			<nav>
+				<ul>
+					
+					<li><a href="#Über-das-Spiel" title="Erfahre mehr über das Spiel">Über das Spiel </a></li>
+					<li><a href="#Mach-mit" title="Mach mit!"> Mach mit beim Spiel!</a></li>
+					<li><a href="#Hilf-uns" title="Spenden">Hilf uns!</a></li>		
+					<li> <a href="#Anzahl-der-Teilnehmer" title="Schau dir die Anzahl der Teilnehmer an">Anzahl der Teilnehmer</a></li>					 		
+ 					<li>	 <a href="#Letzter-Gewinner" title="Gewinner"> Letzter Gewinner</a> 	</li>
+ 					<li><a href="#Credits" title="Inhaber">Credits</a></li>					
+				</ul>
+			</nav>
+
+	</header>
+		<div id="Über-das-Spiel">dasds</div>
+		<section id="container2"> 
+			<h1> Über das Spiel</h1>
+		 Du möchtest mehr Follower, eine größere Reichweite oder eine PSN-Karte? Dann mach jetzt beim Turnier mit, ohne irgendetwas zu bestätigen oder zu zahlen!! Alles, was du machen musst, ist den Namen von deinem Snap- oder Instanamen zu schreiben und dich als Teilnehmer zu registrieren (Wenn du dein Accountnamen nicht angeben willst, dann kannst du auch irgendein Nickname eingeben)! Nach einer Woche wird ein Teilnehmer ausgelost und der Name von dem Gewinner wird auf Platz 1 veröffentlicht. Was erreicht man dadurch? Die anderen Teilnehmer werden dein Account sehen und du wirst vielleicht den einen oder anderen Follower gainen! In der Zukunft wird der Gewinner auch Paysafe/PSN/Google Play Store/Apple Store - Karten gewinnen können, doch dafür müssen mindestens 100 Teilnehmer vorhanden sein. Du kannst deinen Snap/Insta Namen angeben und mit Glück der Gewinner sein. Außerdem wäre es angemessen, eine E-Mail anzugeben, auf die ihr Zugriff habt, da über die Mail euch die Nachricht gesendet wird, ob ihr gewonnen habt oder nicht. Teilt die Seite mit euren Freunden, um noch mehr Spaß zu haben! Wenn ihr mitmachen wollt, dann klickt <a href="#Mach-mit">hier</a>!
+
+		</section>
+		
+
+<div id="Anzahl-der-Teilnehmer">dasds</div>
+<article>
+		
+		<div class="spe">
+			<div class="spe2">
+				<h1> Anzahl der Teilnehmer diese Woche</h1>
+
+			   <p>Woche(  -  ):  </p>   
+			   <br>
+			   <p> zuletzt aktualisiert: 18:11, 28.02.</p> 
+				    <h1> Anzahl der Teilnehmer der letzten Woche</h1>
+				<p> Woche(  -  ): </p><br>
+
+				 <h1>Das nächste Turnier beginnt am 03.04.</h1> 
+				 <p><a href="#Mach-mit">Mach mit!</a></p>
+			</div>
+		</div>
+
+			<div class="spe">
+				<div class="spe2">
+                          <h1>Info</h1>
+                           Links wird die Anzahl der Teilnehmer  gezeigt. Einmal die  <br>
+                           Anzahl von der aktuellen, laufenden Runde und einmal <br>
+                           von der eins vorherigen Runde. Außerdem wird das Datum <br>
+                           gezeigt, damit man einen groben Überblick hat, wann das <br>
+                           letzte Turnier war und wie lang das jetzige Turnier noch <br>
+                           läuft. Die Anzahl der Teilnehmer wird alle 4 Stunden <br>
+						   erhöht, das heißt das es mehr Teilnehmer sein könnten, <br>
+						   als da angezeigt wird. Auch könnt ihr sehen, wann das <br>
+						   nächste Turnier ist. Ihr könnt euch jederzeit für das <br>
+						   Turnier registrieren, da es keine deadline für die <br>
+						   Registration gibt. <br><br><br>
+ 						   <a href="#Mach-mit">Mach mit!</a> <br>
+						  
+						  
+				</div>
+			</div>
+
+</article>
+		<div id="Mach-mit">dasds</div>
+
+<article>
+		<div class="spe">	
+			<div class="spe2">
+		 <form  action="/Viel-Glueck" method="post">
+         	<h1> Mach mit beim Spiel!</h1> <br>
+         	<p><u> Name:</u></p><br>
+         	<input type="text" name="name" placeholder="snap/insta/nick-name" required="" autocomplete="on" method="post">
+         	<p><u>E-mail:</u><span id="sternchen">*</span></p> <br>
+         	<input type="email" name="email" placeholder="example@gmail.com" required="" autocomplete="on" method="post"> <br>
+         	<p id="ema"><span id="sternchen">*</span><u>E-mail wird benötigt, um euch zu benachrichtigen, wenn ihr gewinnt!</u></p>
+			<input type="submit" name="" value="Ich mache mit!">
+         </form>
+         
+         <?php
+          	$con =  mysqli_connect('localhost','turnier','Ibrahimeren7171','turnier');
+           
+            if ($con->connect_error): 
+              echo "no connection dude";
+            endif;
+              
+            if (isset($_POST['submit'])) {
+              $name = $_POST['name'];
+              $email = $_POST['email'];
+              $absenden = $con->prepare("INSERT INTO turnier (name, email) VALUES (?,?)");
+              $absenden->bind_param('ss', $name, $email);
+              $absenden->execute();
+            }   
+           ?>
+          	</div>  	        
+      </div>     
+
+      <div class="spe">
+ 		 <div class="spe2">
+							<h1> Anmeldung zum Turnier</h1>
+				Hier könnt ihr euch für das Turnier anmelden. Es ist, wie bereits in <a href="#Über-das-Spiel">diesem</a> Abschnitt <br>
+				erwähnt, kostenlos und ohne irgendeine Anmeldung! Ihr könnt irgendein Namen eingeben,<br>
+				falls ihr nicht euer Instagram oder Snapchat Namen angeben wollt und eure Mail, damit<br>
+				ihr benachrichtigt werden könnt, falls ihr gewonnen habt oder nicht. Wenn ihr<br>
+				eure echte Mail nicht angeben wollt, dann müsst ihr dies auch nicht tun! Ihr<br>
+				könnt irgendeine fake E-mail angeben, dann bekommt ihr halt keine Benachrichtigung.<br>
+				Dies sollte aber nicht so schlimm sein, denn falls ihr euren Snapchat- oder<br> 
+				Instagramnamen angegeben habt, dann kann ich euch über diese App bescheid geben.<br>
+			    Fall ihr auch nicht euren Snap- oder Instanamen angeben wollt, dann ist dies auch<br> 
+			    kein Problem! Dann wird es halt schwer, euch eine Benachrichtigung zu schicken,<br>
+			    dass ihr gewonnen habt. Die einzige Möglichkeit ist, dass ihr die Seite nach einer<br>
+			    Woche wieder besucht, um zu sehen, wer der <a href="#Letzter-Gewinner">letzte Gewinner</a> war.<br>
+			    Jeder Teilnehmer zählt! Warum? Weil, wenn die Anzahl der Teilnehmer steigt, dann<br> 
+			    werden auch die Preise steigen! Anfangs wird leider nur ein Preis vergeben, und<br> 
+			    zwar der Name des Gewinners. Nach einer bestimmten Zeit werden es auch Preise wie<br>
+			    eine PSN-Karte sein. Ich hoffe euch gefällt mein Konzept und die Art der Umsetzung.<br>
+			    Falls euch mangelhafte Stellen oder bessere Umsetzungsideen einfallen, dann klickt<br>
+			    <a href="#end">hier</a>, um meine E-Mail zu sehen und über die Mail eure Idee mitteilen zu können!
+
+		 </div> 
+	</div>
+</article>
+
+<div id="Hilf-uns">dasds</div>
+<div class="spe" id="spee">  
+<div class="spe2">  <h1>Hilf uns!</h1> <br> <br> Um die Seite weiterhin am Laufen zu halten, würden Spenden in der Größe vom Preis eines Kaffee schon reichen! Warte mal... Das war doch nicht ernst gemeint, wer spendet denn bitte No-Name Seiten? Niemand. Ich selbst würde auch nichts geben. War nur ein kleiner Prank, also chill. Und wenn ihr spenden wollen würdet, spendet es lieber an Organisationen, bei denen ihr euch sicher seid, dass diese das Geld auch für diese Zwecke nutzen. also um den armen Leuten zu helfen. Der Preis einer Tasse Kaffee würde dabei völlig reichen. Ihr müsst euch nur denken, wie es wäre, wenn ihr in derselben Situation wie die armen Leute wärt. So würdet ihr direkt spenden, vertraut mir. Man muss logisch nachdenken und auf diese Erkenntnisse von selbst kommen. Es ist jedem selbst überlassen. </div>
+</div>
+
+<div id="Letzter-Gewinner">dasds</div>
+<div class="spe">
+ 	 <div class="spe2"> <h1><u> Gewinner von der letzten Runde</u></h1> <br>
+ 	 	<span id="winner"> Name:    </span>       <br><br><br>
+ 	 	Respektiert den Gewinner und wenn ihr gottesfürchtig seid, dann addet ihn. Wie gesagt, desto mehr Leute im Turnier mitmachen, desto besser werden die Belohnungen (z.B. 15 Euro Karten). Es ist und wird immer kostenlos bleiben. Wie gesagt, schreibt einfach nur ein Nickname und eine Mail auf die ihr zugreifen könnt, damit ich euch den Preis schicken kann bzw. die Nachricht, dass ihr gewonnen habt.
+	 </div> 
+</div>
+
+<div class="spe">
+ 	 <div class="spe2">
+			<h1>Wie wird der Gewinner ausgewählt?</h1>
+			Von all den Teilnehmern wird der Gewinner random ausgelost. Ich wähle den Gewinner nicht selbst aus! Das wäre unfair gegenüber den Leuten, die ich nicht kenne, da ich mit Absicht die Leute gewinnen lassen würde, die ich kenne. Und das wollen wir NICHT! Deshalb bleibt ganz ruhig, jeder hat die gleiche Wahrscheinlichkeit als Gewinner rauszukommen. Die Wahrscheinlichkeit zum Gewinn für einen Teilnehmer beträgt 1:x (x = Anzahl der Teilnehmer).
+	 </div> 
+</div>
+	
+
+	<div class="spe"> 
+		 <div class="spe2"> <h1>Ein Gefallen an euch</h1><br>
+        Ich würd mich freuen, wenn ihr nicht nur mitmacht, sondern die Seite auch teilt, damit die Teilnehmer immer mehr werden. Wie gesagt, wenn eine bestimmte Teilnehmeranzahl erreicht ist, dann wird der Gewinner sogar eine Paysafe-Karte oder PSN-Karte gewinnen können!! Ich würde es jetzt schon machen, aber ich bin keine Bonze und ich hab kein Sponsor. Ich bitte um euer Verständnis. Kopiert den Link: <span id="hiea">duverliersteh.de</span> und sendet ihn an random Gruppen oder verlinkt den Link in euerer Story. Diese Seite dient nur der Unterhaltung. Ihr müsst euch nicht mal anmelden. Ihr gibt irgendein Namen an und eure Mail (damit ich euch bescheid geben kann, falls ihr gewonnen habt) und ihr seid mit dabei und chillt 1 Woche. Mehr muss man nicht machen. 
+    	 </div>
+	</div>
+
+
+
+<div class="spe">
+	<div class="spe2">
+			<h1>Fitness-Splits</h1>
+			3er Splitt: <br> Tag 1: Brust+Bizeps+Rücken; <br>Tag 2: Beine+Bauch;<br> Tag 3: Trizeps+Schulter <br>
+			Je Einheit, Aufwärmung per Laufband oder Radfahren (10 min.). <br> Wenn ihr schnellstmöglich ein Sixpack haben wollt,<br>
+			dann je Einheit, nach dem Programm nochmals auf der Matte Sixpack-Übungen, zB. Crunches oder Russische Drehungen. Wie diese gehen, sieht ihr <a href="https://youtu.be/wkD8rjkodUI" target="_blank">hier</a>. <a href="https://youtu.be/CWOZyf8kaH0" target="_blank">Jack-Knives</a> sind auch sehr effektiv.
+
+	</div>
+</div>
+
+<div class="spe">
+	<div class="spe2">
+				<h1>Unbezahlte "Werbung"</h1>
+				Gibt zu, dass Samsung besser ist als Iphone, und Huawei besser als Samsung. Samsung ist meiner Meinung nach tot, da die neuen Modelle schon bereits nach einem halben Jahr stark an Wert verlieren, d.h. sie werden nur noch fast zur Hälfte des ursprünglichen Preises verkauft.Iphone ist an sich nicht schlecht (mit Iphone mein ich Apple natürlich), aber dieses Unternehmen zieht die Leute ab in meinen Augen. Sonst sind die Produkte gar nicht schlecht gemacht. Huawei ist wie ein Newcomer in der Rapszene. Huawei bringt derzeit die modernsten und intelligentesten Handys raus und hat somit die Top 1 verdient. Wir fangen mit dem Punkt an, dass die Huawei mate pro Serie eine artificial intelligence, also künstliche Intelligenz, die KI, besitzt. Darüber hinaus verfügt das Handy über viele Funktionen, die Samsung und Iphone vielleicht auch haben, aber paar auch nicht, wie zum Beispiel den <a href="https://www.youtube.com/watch?v=KVjg9WQMvoE" target="_blank">Desktop-Modus</a>.
+	</div>
+</div>
+<article>
+<div class="spe">
+	<div class="spe2">
+		 <h1>Beste Serien</h1>
+		 <a href="https://youtu.be/jrLhP5sK2wI">-The Punisher</a> <br>
+		 <a href="https://youtu.be/4SzFubsoW1M">-Rick and Morty</a> <br>
+		 <a href="https://youtu.be/g4wzLlflvhg">-Family Guy</a> <br>
+		 <a href="https://youtu.be/yK8e0EQE3O0">-The Protector</a> <br>
+		 <a href="https://youtu.be/Qofw470E0-k">-Haus des Geldes</a> (Bald kommt dritte Staffel) <br>
+		 <a href="https://youtu.be/vJAoxBQ6hs0">-Pickpockets:Meister im Stehlen</a> (Film) <br>
+		 Erste Priorität auf Rick & Morty, bisschen pervers an manchen Stellen, <br> aber trotzdem sehr lustig und gut gemacht.
+	</div>
+</div>
+
+<div class="spe">
+	<div class="spe2">
+			<h1>Gute Musik</h1>
+			<a href="https://youtu.be/eFN0Zzflors">-JAJA</a> Fero47 <br>
+			<a href="https://youtu.be/7dh93lkFKv4">-Dogs of Berlin</a> Sinan-G <br>
+			<a href="https://youtu.be/zGEsbNGXYts">-Ausländer</a> Mert <br>
+			<a href="https://youtu.be/ee3BxXFfPNg">-Santa Maria</a> LOKO BEN <br>
+			<a href="https://youtu.be/rCY5sCVTSPA">-u21</a> Mert <br>
+			<a href="https://youtu.be/_7OLff7HYo0">-Drama</a> Bizzey, Boef <br>
+			So eine kleine Top 6 Liste (Geschmackssache). <br>
+			Meine persönliche Top 6.
+	</div>
+</div>
+
+<div class="spe6">
+	<div class="spe2">
+ 			<h4><u>Gelange wieder nach ganz oben</u></h4>
+ 			Ich will wieder <br> nach <a href="#headerr">oben</a>!
+	</div>
+</div>
+</article>
+<div id="Credits">dasds</div>
+		
+
+<h1 id="csa">Credits</h1>
+
+<table  class="tableess" border="1" cellspacing="0">
+	
+	<tr id="tr1">
+		<td> <u>ÜBER MICH:</u>
+	Ich wolte mal eine neue Website erstellen, die nur zur <strong>Unterhaltung</strong> dient. Ich bin ein Hayvan, der sich für das Webdevelopment interessiert und lernt, wie man Websites erstellen kann. Ich mache es für mich selbst, also ohne Zwang. Ich bin noch ein eher unerfahrener Webdeveloper, also falls sich Mängel aufweisen sollten, dann entschuldige ich mich dafür und hoffe, dass ihr Verständnis dafür habt. Ich hoffe euch gefällt die Idee! Teilt die Seite mit euren Freunden! </td>
+	</tr>
+     
+	<tr>
+		<td> <u>STANDORT</u>: Irgendwo in Deutschland, Bayern.</td>
+	</tr>
+
+	<tr>
+		<td><u>FEEDBACK</u>: Falls ihr mangelhafte Stellen entdeckt, dann meldet es sofort bei mir! Danke :) Ich werde versuchen, diese Fehler zu beheben! Meine Mail findet ihr ganz unten unter "Kontakt:"!</td>
+	</tr>
+
+	<tr>
+		<td><u>UPDATES:</u> Chat in der Website (kommt bald) <br> Falls ihr noch was wollt, dann mailt mir.</td>
+	</tr>
+
+</table>
+
+  <br><hr><footer id="end"><br>Kontakt:  lassdirjetzthelfen@gmail.com<br> <br>Versuch dein Bestes! Motivation ist eines der wichtigsten Begriffe, auf die man achten sollte bzw. die vorhanden sein sollten. <br><br> &copy; 2019 <br>
+<br>  HAYVAN MODUS AN
+  </footer>
+</body>
+</html>
